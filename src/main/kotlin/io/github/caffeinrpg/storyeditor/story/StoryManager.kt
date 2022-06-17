a5693c5ee112504e2caaa840403905a800a8e4a9
みにどん <48941476+M1n1don@users.noreply.github.com>
2022-06-17 23:58:48 +0900

    [Add] ストーリー系の関数を一通り書き出し

--- src/main/kotlin/io/github/caffeinrpg/storyeditor/story/StoryManager.kt
+++ src/main/kotlin/io/github/caffeinrpg/storyeditor/story/StoryManager.kt
@@ -1,5 +1,8 @@
 package io.github.caffeinrpg.storyeditor.story
 
+import io.github.caffeinrpg.storyeditor.table.Stories
+import org.bukkit.entity.Player
+
 class StoryManager
 {
     companion object
@@ -13,5 +16,38 @@ class StoryManager
 
             return instance!!
         }
+    }
+
+    val stories: MutableMap<Int, Story> = mutableMapOf()
+    val playing: MutableMap<Player, Stories> = mutableMapOf()
+
+    fun createStory(title: String): Story?
+    {
+        TODO()
+    }
+
+    fun deleteStory(story: Story)
+    {
+        TODO()
+    }
+
+    fun addword(story: Story, line: Int, word: String): Story?
+    {
+        TODO()
+    }
+
+    fun addCommand(story: Story, command: String): Story?
+    {
+        TODO()
+    }
+
+    fun playStory(story: Story, target: Player)
+    {
+        TODO()
+    }
+
+    fun stopStory(target: Player)
+    {
+        TODO()
     }
 }
